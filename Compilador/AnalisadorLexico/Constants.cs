9297ed73949e2c9cf382a64c2feb0d01bdddf102
Caroline <33700047+cregalin@users.noreply.github.com>
2019-04-08 19:22:27 -0300

    Analisador Lexico - Base em java

---
+++ Compilador/AnalisadorLexico/Constants.cs
@@ -0,0 +1,20 @@
+namespace AnalisadorLexico
+{
+    public abstract class Constants : ScannerConstants
+    {
+        int EPSILON = 0;
+        int DOLLAR = 1;
+        int t_identificador = 2;
+        int t_constanteInteira = 3;
+        int t_constanteReal = 4;
+        int t_constanteString = 5;
+        int t_constanteCaractere = 6;
+        int t_bloco = 7;
+        int t_TOKEN_8 = 8; //"+"
+        int t_TOKEN_9 = 9; //"*"
+        int t_TOKEN_10 = 10; //"/"
+        int t_TOKEN_11 = 11; //"?"
+
+    }
+
+}
